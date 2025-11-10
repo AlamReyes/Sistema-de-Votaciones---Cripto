0e9ff538d5b3db332ecfb9ff25cff57f08ecf600
EAJH <90656408+EAJH@users.noreply.github.com>
2025-11-10 16:50:11 -0600

    feature: Página de Login

---
+++ app/(app)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// app/dashboard/page.tsx
+export default function DashboardPage() {
+  return (
+    <div>
+      <h1>Sistema de votaciones</h1>
+      <p>Has iniciado sesión correctamente.</p>
+      <p>Desde aquí puedes navegar a las votaciones y ver los resultados.</p>
+    </div>
+  );
+}
